d86e4f01cf7a58f2b239b63d3fd08c5c4ddcd270
jovandamjanovic <jovandamjanovic@gmail.com>
2016-07-10 21:15:40 +0000

    setup remote mongodb on mlab

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var router = express.Router();
 var db = mongoose.connection;
 
 db.on('error', console.error);
-mongoose.connect('mongodb://localhost/shortUrls');
+mongoose.connect('mongodb://admin:admin@ds013024.mlab.com:13024/shorturls');
 
 router.use(function(req, res, next) {
     next();
